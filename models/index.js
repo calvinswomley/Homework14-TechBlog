2ea004ad4bd9cc9798baa45dbe512534033a2f26
Calvin Swomley <calvinswomley@gmail.com>
2021-12-29 08:14:29 -0500

    models index created

---
+++ models/index.js
@@ -0,0 +1,37 @@
+// model imports
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+//posts belong to users and have many comments
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+//users have many posts & comments
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+//Comments belong to posts & to users
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = {
+    User,
+    Post,
+    Comment,
+};
