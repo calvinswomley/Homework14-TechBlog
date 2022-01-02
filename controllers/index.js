bc4f1c8ba9f47cb72e295d1e412ac012bcdd245f
Calvin Swomley <calvinswomley@gmail.com>
2022-01-02 11:50:26 -0500

    route index setup

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,11 @@
 //include '/posts' as the name of one of the routes (referenced in homepage.handlebars)
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
